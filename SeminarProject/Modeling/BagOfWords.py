5c72b0c2442607324c6b808952115448c49f94c4
BRutan <rutan.benjamin@gmail.com>
2020-07-14 01:53:24 -0400

    Added BagOfWords model to build from scratch.

---
+++ SeminarProject/Modeling/BagOfWords.py
@@ -0,0 +1,16 @@
+##########################################
+# BagOfWords.py
+##########################################
+# Description:
+# * Bag of words NLP model from scratch.
+
+class BagOfWords:
+    """
+    * Bag of words NLP model built from scratch. 
+    """
+    def __init__(self):
+        """
+        * Initialize new model and build
+        using dataset.
+        """
+        pass
